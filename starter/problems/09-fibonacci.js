07d6757aa6cacdf4ebd91c18909562aad222c8e9
Ali <a.k.ezzeddine@gmail.com>
2022-06-22 13:23:08 -0400

    recursiveProject Prob 10

--- starter/problems/09-fibonacci.js
+++ starter/problems/09-fibonacci.js
@@ -24,13 +24,6 @@ base case:
 
 
 
-
-const fibonacci = n => {
-	if (n <= 1) return n;
-	return fibonacci(n-1) + fibonacci(n-2);
-}
-
-
 console.log(fibonacci(1)); // 1
 console.log(fibonacci(2)); // 1
 console.log(fibonacci(3)); // 2
--- starter/problems/10-factorial.js
+++ starter/problems/10-factorial.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a recursive function called `factorial` that takes an integer, `num`,
-and returns the factorial of `num`. Assume the value of `num` is greater 
+and returns the factorial of `num`. Assume the value of `num` is greater
 than or equal to 1.
 
 A factorial is the number get when multiplying a number by itself minus one
@@ -16,12 +16,14 @@ factorial(3); // 6
 factorial(5); // 120
 ***********************************************************************/
 
-// your code here
-  
+const factorial = num => {
+  if (num === 1) return num;
+  return num * factorial(num - 1)
+}
+console.log(factorial(5)); // 120
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = factorial;
 } catch (e) {
   module.exports = null;
 }
-  
